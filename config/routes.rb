ca74473dc85ab40ba779850e9f48923dea5c76b0
ArBuZzZ44 <zahar1702@mail.ru>
2022-12-19 03:15:38 +0700

    change /contacts/new to /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  resource :contacts, only: [:new, :create] #resorce - метод, который принимаем символ и хеш. Используем только методы new и create на стр.
+  #либо get 'contacts' => 'contacts#new'
+  resource :contacts, only: [:new, :create], path_names: {:new => ''} #resorce - метод, который принимаем символ и хеш. Используем только методы new и create на стр.
   resources :articles #Если сущность одна, то resource. А если собираемся использовать несколько сущностей, то resources(много экземпляров сущности)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
