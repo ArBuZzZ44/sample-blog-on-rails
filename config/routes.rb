93c2e869472ec00c6fa6ab7d748c3b8283f909fb
ArBuZzZ44 <zahar1702@mail.ru>
2022-12-19 00:59:54 +0700

    save contact on submit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   resource :contacts, only: [:new, :create] #resorce - метод, который принимаем символ и хеш. Используем только методы new и create на стр.
-  resources :articles
+  resources :articles #Если сущность одна, то resource. А если собираемся использовать несколько сущностей, то resources(много экземпляров сущности)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
