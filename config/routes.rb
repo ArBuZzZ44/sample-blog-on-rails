2ad0fab432be8c1da53deed35ec666683d27f843
ArBuZzZ44 <zahar1702@mail.ru>
2022-12-19 20:46:42 +0700

    add page about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'terms/new'
+  get 'about/new'
 
   #либо get 'contacts' => 'contacts#new'
   resource :contacts, only: [:new, :create], path_names: {:new => ''} #resorce - метод, который принимаем символ и хеш. Используем только методы new и create на стр.
