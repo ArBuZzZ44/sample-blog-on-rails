edfedcab7ba698fd32108aba03aed316ee608361
ArBuZzZ44 <zahar1702@mail.ru>
2022-12-23 18:54:21 +0700

    create action comment#create

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   #либо get 'contacts' => 'contacts#new'
   resource :contacts, only: [:new, :create], path_names: {:new => ''} #resorce - метод, который принимаем символ и хеш. Используем только методы new и create на стр.
-  resources :articles #Если сущность одна, то resource. А если собираемся использовать несколько сущностей, то resources(много экземпляров сущности)
+  resources :articles do
+    resources :comments 
+  end   #Если сущность одна, то resource. А если собираемся использовать несколько сущностей, то resources(много экземпляров сущности)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
