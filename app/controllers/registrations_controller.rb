3e90a55cc07008e2dd670cbc22b4cfbd7e683eaa
ArBuZzZ44 <zahar1702@mail.ru>
2022-12-26 00:41:52 +0700

    show username in header

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def after_sign_up_path_for(resource)
-    root_path # or any other path
+    root_path # or any other path # Решает проблему с маршрутом sign up, после добавляем маршрут с контроллером
   end
 end
