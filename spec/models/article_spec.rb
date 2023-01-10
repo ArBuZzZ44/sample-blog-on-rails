0b457ccd2f972263e5d2c76d18373c1c7d0baa8c
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-10 14:45:29 +0700

    update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,14 @@ describe Article do
 	describe "associations" do 
 		it {should have_many :comments} #Для тестирования has_many используется have_many из-за грамматики
 	end
+
+	describe "#subject" do 
+		it "returns the article title" do 
+			#Создаем объект article хитрым способом
+			article = create(:article, title: 'Lorem ipsum') #Создаем объект с помощью factoryBot, не трогая базу данных
+			
+			#Assert, проверка
+			expect(article.subject).to eq 'Lorem ipsum'
+		end
+	end
 end
