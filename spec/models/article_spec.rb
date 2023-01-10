775251c4b29410ef20a0f4c03b67e0da6ad2219c
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-10 12:45:18 +0700

    update article spec to include validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 describe Article do 
-	it {should have_many :comments} #Для тестирования has_many используется have_many из-за грамматики
+	describe "validations" do
+		it {should validate_presence_of :title}
+		it {should validate_presence_of :text}
+	end
+
+	describe "associations" do 
+		it {should have_many :comments} #Для тестирования has_many используется have_many из-за грамматики
+	end
 end
