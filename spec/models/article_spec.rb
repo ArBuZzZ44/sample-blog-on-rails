13b60c879283d43f367efa7bcfd32da6a1a4f2dc
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-12 03:30:42 +0700

    update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,7 @@ describe Article do
 	describe "validations" do
 		it {should validate_presence_of :title}
 		it {should validate_presence_of :text}
+		it {should validate_length_of(:title).is_at_most(140)}
 	end
 
 	describe "associations" do 
@@ -22,6 +23,11 @@ describe Article do
 
 	describe "#last_comment" do 
 		it "returns the last comment" do 
+			# создаем статью с комментариями
+			article = create(:article_with_comments)
+
+			# проверка 
+			expect(article.last_comment.body).to eq "comment body 3"
 		end
 	end
 end
