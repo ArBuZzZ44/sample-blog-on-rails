bd4bb19f4e5fed32664939e36b52fbfd940fc209
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-12 03:29:27 +0700

    add length validation for the title

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments
-  validates :title, presence: true
+  validates :title, presence: true, length: {maximum: 140}
   validates :text, presence: true
 
   def subject
