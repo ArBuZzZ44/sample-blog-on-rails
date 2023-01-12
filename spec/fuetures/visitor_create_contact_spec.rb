4e6a1d232fe9c5c41152e8245370992d0e498411
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-12 22:27:12 +0700

    update create contact acceptance test

--- spec/fuetures/visitor_create_contact_spec.rb
+++ spec/fuetures/visitor_create_contact_spec.rb
@@ -4,6 +4,6 @@ feature "Contact Creation" do
 	scenario "allows accees to contacts page" do 
 		visit '/contacts'
 
-		expect(page).to have_content 'Contact us'
+		expect(page).to have_content I18n.t('contacts.contact_us') #ищет текст на сгенерированной странице
 	end
 end
