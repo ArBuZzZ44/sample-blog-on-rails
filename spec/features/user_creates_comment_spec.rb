902c01769054489644f4e72e5c557665d4f31c8e
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-16 20:07:32 +0700

    update spec comment article

--- spec/features/user_creates_comment_spec.rb
+++ spec/features/user_creates_comment_spec.rb
@@ -3,19 +3,16 @@ require "rails_helper"
 feature "Article Creation" do 
   before(:each) do 
     sign_up
+    create_article
   end
 
-  scenario "allows user to visit article#index" do
-    create_article
-
+  scenario "allows user to visit article#index" do  
     visit articles_path
 
     expect(page).to have_content 'Added at'
   end
 
   scenario "allows user to click on the link Show" do
-    create_article
-    
     visit articles_path
 
     find_link('Show').click
