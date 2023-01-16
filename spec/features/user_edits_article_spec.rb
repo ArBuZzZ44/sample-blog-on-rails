8421064c78d92cd078247f709873c7452eed18f5
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-17 00:23:04 +0700

    update spec edit article

--- spec/features/user_edits_article_spec.rb
+++ spec/features/user_edits_article_spec.rb
@@ -11,6 +11,12 @@ feature "Article Creation" do
 
     find_link('Edit').click
 
-    expect(page).to have_current_path((edit_article_path(Article.last)))
+    expect(page).to have_current_path(edit_article_path(Article.last))
+  end
+
+  scenario "allows user to edit the article" do 
+    visit edit_article_path(Article.last)
+
+    expect(page).to have_content 'Edit article'
   end
 end
