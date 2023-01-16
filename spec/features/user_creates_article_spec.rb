827a1259977b935537b0a4f0e751a4357997a34a
ArBuZzZ44 <zahar1702@mail.ru>
2023-01-16 18:55:33 +0700

    spec for create article

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -9,4 +9,13 @@ feature "Article Creation" do
     visit new_article_path
     expect(page).to have_content 'New article'
   end
+
+  scenario "allows user to create article" do
+    visit new_article_path
+
+    fill_in :article_title, :with => 'titleexample'
+    fill_in :article_text, :with => 'textforarticle'
+
+    click_button 'Save Article'
+  end
 end
